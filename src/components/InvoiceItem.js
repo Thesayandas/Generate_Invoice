9e414e5e5b6a2d41d3add4237f1d85d8b5dd46ba
Thesayandas <142918700+Thesayandas@users.noreply.github.com>
2025-11-05 00:29:29 +0530

    Update InvoiceItem.js

--- src/components/InvoiceItem.js
+++ src/components/InvoiceItem.js
@@ -27,7 +27,7 @@ class InvoiceItem extends React.Component {
         {/* ====== HEADER WITH COMPANY LOGO ====== */}
         <div className="d-flex align-items-center mb-3 border-bottom pb-2">
           <img
-            src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhx5qd_VIv2EjrNhDrQYwr_Gw5OyS-wOVwNMRcADwyIWTyHOMU1NrSJAajQtH8Vsg1h0H1CNoGWlV7cs5FSNegTsjOtwzco14dCnp6ZiOc5mlxAHjT0y6lZS6KHqbOg53RsnQT7kFE4F9IJf0J77iOrowvt9xKX9CqdbsgJsxp60pfxcG_w-tWWsbHYHwal/s200/TIAS%20Logo.png"
+            src="./main/src/TIAS%20Logo.png"
             alt="TIAS Logo"
             style={{ height: "60px", marginRight: "15px" }}
           />
